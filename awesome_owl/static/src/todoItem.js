0aeb31474155b1aff260b6eecdd51d2c92895f43
nahidtanvir <71749007+nahidtanvir@users.noreply.github.com>
2025-05-05 14:47:05 +0600

    ch1 task 12

--- awesome_owl/static/src/todoItem.js
+++ awesome_owl/static/src/todoItem.js
@@ -5,7 +5,11 @@ import { Component, xml } from "@odoo/owl";
 export class TodoItem extends Component {
     static template = xml`
         <div class="d-inline-block" t-att-class="{'text-muted text-decoration-line-through': props.todo.isCompleted}">
-            <h6><t t-esc="props.todo.id"/>. <t t-esc="props.todo.description"/></h6>
+            <h6>
+                <input class="me-2" type="checkbox" t-att-checked="props.todo.isCompleted" t-on-change="()=>props.toggleState(props.todo.id)" />
+                <t t-esc="props.todo.id"/>. <t t-esc="props.todo.description"/>
+                <span class="fa fa-remove ms-2 text-danger" t-on-click="()=>props.removeTodo(props.todo.id)"/>
+            </h6>
         </div>
     `
 
@@ -14,6 +18,8 @@ export class TodoItem extends Component {
             id: Number,
             description: String,
             isCompleted: Boolean
-        }
+        },
+        toggleState: Function,
+        removeTodo: Function,
     }
 }
--- awesome_owl/static/src/todoList.js
+++ awesome_owl/static/src/todoList.js
@@ -1,17 +1,22 @@
-import { Component, useState, xml } from "@odoo/owl"
+import { Component, useState, xml, useRef, onMounted } from "@odoo/owl"
 import { TodoItem } from "./todoItem"
 
 export class TodoList extends Component {
     static template = xml`
-        <input type="text" placeholder="Add a todo" t-on-keyup="addTodo" />
+        <input type="text" placeholder="Add a todo" t-on-keyup="addTodo" t-ref="input" />
         <p t-foreach="todos" t-as="item" t-key="item.id" class="m-2">
-            <TodoItem todo="item" />
+            <TodoItem todo="item" toggleState="(id)=>this.toggleStates(id)" removeTodo="(id)=>this.removeTodo(id)" />
         </p>
     `
     static components = { TodoItem };
 
     setup() {
         this.todos = useState([]);
+        this.next_id = useState({'value': 1});
+        this.inputRef = useRef('input')
+        onMounted(() => {
+          this.inputRef.el.focus()
+       });
     }
 
     addTodo(ev) {
@@ -19,11 +24,30 @@ export class TodoList extends Component {
             const value = ev.target.value.trim();
             if (value) {
                 this.todos.push({
-                    id: this.todos.length + 1,
+                    id: this.next_id.value,
                     description: value,
                     isCompleted: false
                 });
                 ev.target.value = '';
+                this.next_id.value += 1
+            }
+        }
+    }
+
+    toggleStates(id) {
+        for (const todo of this.todos) {
+            if (todo?.id === id) {
+                todo.isCompleted = !todo.isCompleted
+                break;
+            }
+        }
+    }
+
+    removeTodo(id) {
+        console.log(id)
+        for (let i = 0; i < this.todos.length; i++ ) {
+            if (this.todos[i].id === id) {
+                this.todos.splice(i, 1)
             }
         }
     }
