0385b40e682ed7eca1dd11f48a2b460f81201311
nahidtanvir <71749007+nahidtanvir@users.noreply.github.com>
2025-05-11 18:35:37 +0600

    dashboard item added

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,12 +1,37 @@
-/** @odoo-module **/
-
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { Layout } from "@web/search/layout";
+import { Layout, onWillStart } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { rpc } from "@web/core/network/rpc";
+import { DashboardItem } from "./dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
+
+    setup() {
+      this.action = useService("action");
+      // onWillStart(async () => {
+      //     const result = await rpc("/awesome_dashboard/statistics");
+      //     print(result)
+      //  })
+    }
+
+    openCustomerView() {
+      this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
