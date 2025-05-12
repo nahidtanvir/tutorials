f00420bf929fed0dc8e53c02573ec10d9466476f
nahidtanvir <71749007+nahidtanvir@users.noreply.github.com>
2025-05-12 15:08:39 +0600

    chart added

---
+++ awesome_dashboard/static/src/pie_chart.js
@@ -0,0 +1,42 @@
+import { Component, onWillStart, onMounted, useState, useRef, onWillUnmount } from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+import { getColor } from "@web/core/colors/colors";
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+    static props = {data: Object};
+
+    setup() {
+        this.canvasRef = useRef("canvas");
+        this.chart = null;
+        this.state = useState({
+            loading: true,
+            error: false
+        });
+
+        onWillStart(() => loadJS("/web/static/lib/Chart/Chart.js"));
+        onMounted(() => {
+            this.renderChart();
+        });
+        onWillUnmount(() => {
+            this.chart.destroy();
+        });
+    }
+
+    renderChart() {
+        this.chart = new Chart(this.canvasRef.el, {
+            type: 'pie',
+            data: {
+                labels: Object.keys(this.props.data),
+                datasets: [{
+                    data: Object.values(this.props.data),
+                    backgroundColor: Object.values(this.props.data).map((_, index) => getColor(index)),
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+}
